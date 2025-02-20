df08ec561ee2de9b571e734093a544dd8484b9ab
erick-tpx1 <javier1996.ec@gmail.com>
2025-02-20 13:22:24 +0100

    allow host

--- sale_system/sale_system/settings.py
+++ sale_system/sale_system/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = Config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = Config('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
